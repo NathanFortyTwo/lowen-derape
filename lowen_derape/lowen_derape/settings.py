59140acb1b59310ba0c91c4c211c243418d7e62b
Nathan Feret <nathan.feret@enioka.com>
2025-11-03 19:48:47 +0100

    frontend boost

--- lowen_derape/lowen_derape/settings.py
+++ lowen_derape/lowen_derape/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
